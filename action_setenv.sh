48ce3d15f84f23a7c72a7b9c9687eecf72c212bf
vaanye <xiaochoudidai@gmail.com>
2021-01-08 18:52:35 +0800

    Update action_setenv.sh

--- action_setenv.sh
+++ action_setenv.sh
@@ -2,8 +2,8 @@
 
 function set_env() { echo "$1=$2" >> $GITHUB_ENV; }
 
-APP_NAME="legado"
-APP_GIT_URL="https://github.com/gedoor/legado.git"
+APP_NAME="muxin"
+APP_GIT_URL="https://github.com/vaanye/MyBookshelf.git"
 APP_SUFFIX="A"
 
 if [ $SECRETS_ENABLE = 'true' ] && [ -n "$SECRETS_APP_NAME" ] && [ -n "$SECRETS_GIT_URL" ] && [ $REPO_ACTOR = $REPO_OWNER ]; then
